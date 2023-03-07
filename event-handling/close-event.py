d1c626eadbf88104eaa3df7efb7887b97abf7c2c
rifatrakib <Rakib.1508@outlook.com>
2023-03-07 10:07:29 +0600

    show connecting events that occur when figure closes

---
+++ event-handling/close-event.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def on_close(event):
+    print("Closed Figure!")
+
+
+fig = plt.figure()
+fig.canvas.mpl_connect("close_event", on_close)
+
+plt.text(0.35, 0.5, "Close Me!", dict(size=30))
+plt.show()
