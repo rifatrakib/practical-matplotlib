dbdddff0036c090b90dc08cca76b7cda377d9e48
rifatrakib <Rakib.1508@outlook.com>
2023-03-24 12:47:19 +0600

    customizing rcParams on the fly

---
+++ miscellaneous/customize-rc.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+plt.subplot(311)
+plt.plot([1, 2, 3])
+
+# the axes attributes need to be set before the call to subplot
+plt.rcParams.update({
+    "font.weight": "bold",
+    "xtick.major.size": 5,
+    "xtick.major.pad": 7,
+    "xtick.labelsize": 15,
+    "grid.color": "0.5",
+    "grid.linestyle": "-",
+    "grid.linewidth": 5,
+    "lines.linewidth": 2,
+    "lines.color": "g",
+})
+plt.subplot(312)
+plt.plot([1, 2, 3])
+plt.grid(True)
+
+plt.rcdefaults()
+plt.subplot(313)
+plt.plot([1, 2, 3])
+plt.grid(True)
+plt.show()
