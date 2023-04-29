7893208d9e1b5b368e4442f41ee43c0e07a02997
rifatrakib <Rakib.1508@outlook.com>
2023-04-29 11:37:52 +0600

    visualizing values of 2D array with hinton diagram

---
+++ specialty-plots/hinton.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def hinton(matrix, max_weight=None, ax=None):
+    """Draw Hinton diagram for visualizing a weight matrix."""
+    ax = ax if ax is not None else plt.gca()
+
+    if not max_weight:
+        max_weight = 2 ** np.ceil(np.log2(np.abs(matrix).max()))
+
+    ax.patch.set_facecolor("gray")
+    ax.set_aspect("equal", "box")
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
+    for (x, y), w in np.ndenumerate(matrix):
+        color = "white" if w > 0 else "black"
+        size = np.sqrt(abs(w) / max_weight)
+        rect = plt.Rectangle([x - size / 2, y - size / 2], size, size, fc=color, ec=color)
+        ax.add_patch(rect)
+
+    ax.autoscale_view()
+    ax.invert_yaxis()
+
+
+if __name__ == "__main__":
+    # Fixing random state for reproducibility
+    np.random.seed(19680801)
+
+    hinton(np.random.rand(20, 20) - 0.5)
+    plt.show()
