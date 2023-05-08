293fff1870f6e4c82636331f25671089203c2f79
rifatrakib <Rakib.1508@outlook.com>
2023-05-08 17:12:53 +0600

    demonstrate formatting date into different styles

---
+++ ticks/date-convert.py
@@ -0,0 +1,24 @@
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+import numpy as np
+
+date1 = datetime.datetime(2000, 3, 2)
+date2 = datetime.datetime(2000, 3, 6)
+delta = datetime.timedelta(hours=6)
+dates = drange(date1, date2, delta)
+
+y = np.arange(len(dates))
+
+fig, ax = plt.subplots()
+ax.plot(dates, y ** 2, "o")
+
+ax.set_xlim(dates[0], dates[-1])
+ax.xaxis.set_major_locator(DayLocator())
+ax.xaxis.set_minor_locator(HourLocator(range(0, 25, 6)))
+ax.xaxis.set_major_formatter(DateFormatter("%Y-%m-%d"))
+
+ax.fmt_xdata = DateFormatter("%Y-%m-%d %H:%M:%S")
+fig.autofmt_xdate()
+
+plt.show()
