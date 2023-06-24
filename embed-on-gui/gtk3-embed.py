4f2926d21ef95c5517c260a2a28b555206ee7661
rifatrakib <Rakib.1508@outlook.com>
2023-06-24 12:50:14 +0600

    embedding in GTK4 and modify GTK3 implementations

--- embed-on-gui/gtk3-embed.py
+++ embed-on-gui/gtk3-embed.py
@@ -1,9 +1,7 @@
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from matplotlib.backends.backend_gtk3 import (
-    NavigationToolbar2GTK3 as NavigationToolbar)
 from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
@@ -15,21 +13,19 @@ win.set_default_size(400, 300)
 win.set_title("Embedding in GTK3")
 
 fig = Figure(figsize=(5, 4), dpi=100)
-ax = fig.add_subplot(1, 1, 1)
+ax = fig.add_subplot()
 t = np.arange(0.0, 3.0, 0.01)
-s = np.sin(2*np.pi*t)
+s = np.sin(2 * np.pi * t)
 ax.plot(t, s)
 
-vbox = Gtk.VBox()
-win.add(vbox)
+sw = Gtk.ScrolledWindow()
+win.add(sw)
+# A scrolled window border goes outside the scrollbars and viewport
+sw.set_border_width(10)
 
-# Add canvas to vbox
 canvas = FigureCanvas(fig)  # a Gtk.DrawingArea
-vbox.pack_start(canvas, True, True, 0)
-
-# Create toolbar
-toolbar = NavigationToolbar(canvas)
-vbox.pack_start(toolbar, False, False, 0)
+canvas.set_size_request(800, 600)
+sw.add(canvas)
 
 win.show_all()
 Gtk.main()
