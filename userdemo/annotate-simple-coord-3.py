4eabfacdbfd737b2f211a5917596790c6c534fc1
rifatrakib <Rakib.1508@outlook.com>
2023-07-18 20:39:39 +0600

    annotate with respect to an existing axes

---
+++ userdemo/annotate-simple-coord-3.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from matplotlib.text import OffsetFrom
+
+fig, ax = plt.subplots(figsize=(3, 2))
+an1 = ax.annotate(
+    "Test 1", xy=(0.5, 0.5), xycoords="data",
+    va="center", ha="center",
+    bbox=dict(boxstyle="round", fc="w"),
+)
+
+offset_from = OffsetFrom(an1, (0.5, 0))
+an2 = ax.annotate(
+    "Test 2", xy=(0.1, 0.1), xycoords="data",
+    xytext=(0, -10), textcoords=offset_from,
+    va="top", ha="center",
+    bbox=dict(boxstyle="round", fc="w"),
+    arrowprops=dict(arrowstyle="->"),
+)
+
+plt.show()
